7d0db19e6929250c5b043232667ebabd5fdb00d8
Asim-hameed <asimhameed31@gmail.com>
2023-03-31 19:29:50 +0500

    Add project files.

---
+++ Indexers_C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Indexers_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
