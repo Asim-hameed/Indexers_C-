7a9187a282950afde2462b4e606254d6b940e885
Asim-hameed <asimhameed31@gmail.com>
2023-03-31 19:47:07 +0500

    Example of Indexer

--- Indexers_C#/Program.cs
+++ Indexers_C#/Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace Indexers_C_
 {
-    internal class Program
+    class Employee
+    {
+        private int[] age = new int[3];
+
+
+        // this replaced by object name in main
+        // index replaced by object index in main
+        // value replaced by value given to object in main 
+        public int this[int index]
+        {
+            set 
+            {
+                if (index >= 0 && index < age.Length)
+                {
+
+
+                    if (value > 0)
+                    {
+                        age[index] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Value is invalid !!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Index");
+                }
+            }
+            get 
+            {
+                return age[index];    
+            }
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Employee employee = new Employee();
+            employee[0] = 5;
+            Console.WriteLine(employee[0]);
+
         }
     }
 }
